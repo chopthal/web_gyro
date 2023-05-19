2488faed03497b0db30f72f5f6f1fa4998626559
chopthal <tjckdejr1@gmail.com>
2023-05-19 15:15:04 +0900

    Feat : x plot

--- gyro.js
+++ gyro.js
@@ -2,35 +2,35 @@ const gyroX = document.querySelector(".gyro-x");
 const gyroY = document.querySelector(".gyro-y");
 const gyroZ = document.querySelector(".gyro-z");
 const sensorText = document.querySelector(".sensor");
-const permissionBtn = document.querySelector(".permission-btn");
-
-permissionBtn.addEventListener("click", onClick);
+const startBtn = document.querySelector(".start-btn");
+const stopBtn = document.querySelector(".stop-btn");
+let isStop = false;
+let x = 0;
+startBtn.addEventListener("click", onClickStart);
+stopBtn.addEventListener("click", () => {
+  isStop = true;
+});
 
 window.addEventListener("devicemotion", handleMotion, true);
 
 function handleMotion(event) {
-  // console.log(alpha);
-  // console.log(beta);
-  // console.log(gamma);
-
   console.log(event.acceleration.x);
-
   sensorText.innerHTML = "Reading...";
+
+  x = event.acceleration.x;
 
   gyroX.innerHTML = event.acceleration.x;
   gyroY.innerHTML = event.acceleration.y;
   gyroZ.innerHTML = event.acceleration.z;
-  // Do stuff...
 }
 
-function onClick() {
-  console.log("clicked!");
+function onClickStart() {
   if (typeof DeviceMotionEvent.requestPermission === "function") {
     // Handle iOS 13+ devices.
     DeviceMotionEvent.requestPermission()
       .then((state) => {
         if (state === "granted") {
-          window.addEventListener("devicemotion", handleOrientation);
+          window.addEventListener("devicemotion", handleMotion);
         } else {
           console.error("Request to access the orientation was rejected");
         }
@@ -38,68 +38,29 @@ function onClick() {
       .catch(console.error);
   } else {
     // Handle regular non iOS 13+ devices.
-    window.addEventListener("devicemotion", handleOrientation);
+    window.addEventListener("devicemotion", handleMotion);
   }
+  const interval = setInterval(function () {
+    Plotly.extendTraces(
+      "graph",
+      {
+        y: [[x]],
+      },
+      [0]
+    );
+
+    if (isStop === true) clearInterval(interval);
+  }, 300);
 }
 
 function rand() {
   return Math.random();
 }
 
-var time = new Date();
-
-var trace1 = {
-  x: [],
-  y: [],
-  mode: "lines",
-  line: {
-    color: "#80CAF6",
-    shape: "spline",
+Plotly.plot("graph", [
+  {
+    y: [],
+    mode: "lines",
+    line: { color: "#80CAF6" },
   },
-};
-
-var trace2 = {
-  x: [],
-  y: [],
-  xaxis: "x2",
-  yaxis: "y2",
-  mode: "lines",
-  line: { color: "#DF56F1" },
-};
-
-var layout = {
-  xaxis: {
-    type: "date",
-    domain: [0, 1],
-    showticklabels: false,
-  },
-  yaxis: { domain: [0.6, 1] },
-  xaxis2: {
-    type: "date",
-    anchor: "y2",
-    domain: [0, 1],
-  },
-  yaxis2: {
-    anchor: "x2",
-    domain: [0, 0.4],
-  },
-};
-
-var data = [trace1, trace2];
-
-Plotly.plot("graph", data, layout);
-
-var cnt = 0;
-
-var interval = setInterval(function () {
-  var time = new Date();
-
-  var update = {
-    x: [[time], [time]],
-    y: [[rand()], [rand()]],
-  };
-
-  Plotly.extendTraces("graph", update, [0, 1]);
-
-  if (cnt === 100) clearInterval(interval);
-}, 1000);
+]);
