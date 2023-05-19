16732aad01cbb75b61c26fc70db2a2d899a9a77d
chopthal <tjckdejr1@gmail.com>
2023-05-19 14:29:55 +0900

    alpha

--- gyro.js
+++ gyro.js
@@ -1,69 +1,56 @@
-let sensor = new Gyroscope();
-sensor.start();
+window.addEventListener("deviceorientation", handleOrientation);
 
-const gyroX = document.querySelector(".gyro-x");
-const gyroY = document.querySelector(".gyro-y");
-const gyroZ = document.querySelector(".gyro-z");
-const sensorText = document.querySelector(".sensor");
+function handleOrientation(event) {
+  const alpha = event.alpha;
+  const beta = event.beta;
+  const gamma = event.gamma;
 
-sensor.onreading = () => {
-  console.log("Angular velocity around the X-axis " + sensor.x);
-  console.log("Angular velocity around the Y-axis " + sensor.y);
-  console.log("Angular velocity around the Z-axis " + sensor.z);
+  console.log(alpha);
+  console.log(beta);
+  console.log(gamma);
+  // Do stuff...
+}
 
-  console.log("Reading...");
-  sensorText.innerHTML = "Reading...";
+function onClick() {
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    // Handle iOS 13+ devices.
+    DeviceMotionEvent.requestPermission()
+      .then((state) => {
+        if (state === "granted") {
+          window.addEventListener("devicemotion", handleOrientation);
+        } else {
+          console.error("Request to access the orientation was rejected");
+        }
+      })
+      .catch(console.error);
+  } else {
+    // Handle regular non iOS 13+ devices.
+    window.addEventListener("devicemotion", handleOrientation);
+  }
+}
 
-  gyroX.innerHTML = sensor.x;
-  gyroY.innerHTML = sensor.y;
-  gyroZ.innerHTML = sensor.z;
-};
+// let sensor = new Gyroscope();
+// sensor.start();
 
-sensor.onerror = (event) => {
-  console.log(event.error.name, event.error.message);
-  sensorText.innerHTML = event.error.message;
-};
-
-// // DeviceMotion 이벤트
-// window.addEventListener("devicemotion", devicemotionHandler);
 // const gyroX = document.querySelector(".gyro-x");
 // const gyroY = document.querySelector(".gyro-y");
 // const gyroZ = document.querySelector(".gyro-z");
+// const sensorText = document.querySelector(".sensor");
 
-// // 가속도 변화
-// function devicemotionHandler(event) {
-//   // 가속도
-//   // X축
-//   const x = event.acceleration.x;
-//   // Y축
-//   const y = event.acceleration.y;
-//   // Z축
-//   const z = event.acceleration.z;
-//   console.log(`Angular velocity along the X-axis ${x}`);
-//   console.log(`Angular velocity along the Y-axis ${y}`);
-//   console.log(`Angular velocity along the Z-axis ${z}`);
+// sensor.onreading = () => {
+//   console.log("Angular velocity around the X-axis " + sensor.x);
+//   console.log("Angular velocity around the Y-axis " + sensor.y);
+//   console.log("Angular velocity around the Z-axis " + sensor.z);
 
-//   gyroX.innerHTML = x;
-//   gyroY.innerHTML = y;
-//   gyroZ.innerHTML = z;
-// }
+//   console.log("Reading...");
+//   sensorText.innerHTML = "Reading...";
 
-// // let gyroscope = new Gyroscope({ frequency: 60 });
-// // const gyroX = document.querySelector(".gyro-x");
-// // const gyroY = document.querySelector(".gyro-y");
-// // const gyroZ = document.querySelector(".gyro-z");
+//   gyroX.innerHTML = sensor.x;
+//   gyroY.innerHTML = sensor.y;
+//   gyroZ.innerHTML = sensor.z;
+// };
 
-// // gyroX.innerHTML = "X";
-// // gyroY.innerHTML = "Y";
-// // gyroZ.innerHTML = "Z";
-
-// // gyroscope.addEventListener("reading", (e) => {
-// //   console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
-// //   console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
-// //   console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
-
-// //   gyroX.innerHTML = gyroscope.x;
-// //   gyroY.innerHTML = gyroscope.y;
-// //   gyroZ.innerHTML = gyroscope.z;
-// // });
-// // gyroscope.start();
+// sensor.onerror = (event) => {
+//   console.log(event.error.name, event.error.message);
+//   sensorText.innerHTML = event.error.message;
+// };
